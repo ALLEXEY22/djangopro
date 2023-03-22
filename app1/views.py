cfc7accd0334343445019ee301e0ae0480921de3
фÐalx <alex@vov.su>
2023-03-22 13:57:14 +0300

    add worker list on frontend

--- app1/views.py
+++ app1/views.py
@@ -4,39 +4,46 @@ from app1.models import Worcker
 
 
 def index_page(request):
-    """
-worcker_to_cange = Worcker.objects.get(id=5)    доступ к объекту БД по id запись в переменную
-    print(worcker_to_cange)                     печать переменной в теминале
-    """
+    text = 'моя дядя...'
+    text1 = {'data': 'клуб дедов', 'data2': 'привет телезрители'}
     all_worckers = Worcker.objects.all()
     print(all_worckers)
 
-    """
-    worcker_to_cange.second_name = 'Задира'     изменения 
-    worcker_to_cange.save()
-    """
+    #    return render(request, 'index.html', context={'data': text1})
+    return render(request, 'index.html', {'data': all_worckers})
 
-    """
-    all_worckers = Worcker.objects.all()                        получение списка всех строк из БД QuerySet
-    print(all_worckers)                                         печать переменной в теминале
-    """
-
-    """
-    worckers_filtered = Worcker.objects.filter(salary=60000)    получение отфильтрованных значений из БД
-    print(worckers_filtered)                                    печать переменной в теминале
-    """
-
-    """
-    all_worckers = Worcker.objects.all()                                                получение значения полей
-    for i in all_worckers:
-        print(i.second_name, i.name}, i.salary, i.id})
-        print(f'Фамилия:{i.second_name}, Имя:{i.name}, зарплата:{i.salary}, ID:{i.id}')
-    """
-
-    """
-    Worcker.objects.get(id=5).delete                            удаление по id
-    """
-    return render(request, 'index.html')
 
 def about_page(request):
     return render(request, 'about.html')
+
+
+"""
+worcker_to_cange = Worcker.objects.get(id=5)    доступ к объекту БД по id запись в переменную
+    print(worcker_to_cange)                     печать переменной в теминале
+    """
+
+"""
+   worcker_to_cange.second_name = 'Задира'     изменения 
+   worcker_to_cange.save()
+   """
+
+"""
+all_worckers = Worcker.objects.all()                        получение списка всех строк из БД QuerySet
+print(all_worckers)                                         печать переменной в теминале
+"""
+
+"""
+worckers_filtered = Worcker.objects.filter(salary=60000)    получение отфильтрованных значений из БД
+print(worckers_filtered)                                    печать переменной в теминале
+"""
+
+"""
+all_worckers = Worcker.objects.all()                                                получение значения полей
+for i in all_worckers:
+    print(i.second_name, i.name}, i.salary, i.id})
+    print(f'Фамилия:{i.second_name}, Имя:{i.name}, зарплата:{i.salary}, ID:{i.id}')
+"""
+
+"""
+Worcker.objects.get(id=5).delete                            удаление по id
+"""
